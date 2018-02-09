f633d59526f1416d001640a53dbb8b760d240cbb
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-02-09 10:04:58 +0700

    menambahkan interface

---
+++ SampleAPIClient/SampleAPIClient/Data/IRestServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleAPIClient.Data
+{
+    interface IRestServices<T>
+    {
+        Task<List<T>> RefreshDataAsync();
+        Task SaveItemAsync(T obj, bool isNewItem);
+        Task DeleteItemAsync(string id);
+    }
+}
