d15900997886671e7c362dc3c9d96c914721df3a
Erick Kurniawan <erick.kurniawan@gmail.com>
2018-02-09 10:00:21 +0700

    menambahkan constans

---
+++ SampleAPIClient/SampleAPIClient/Constans.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAPIClient
+{
+    public static class Constans
+    {
+        public static string RestUrl = @"http://pupukindoserver.azurewebsites.net/";
+    }
+}
---
+++ SampleAPIClient/SampleAPIClient/Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleAPIClient.Models
+{
+    public class Course
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+    }
+}
